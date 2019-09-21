81267205c8b331a78c01b12dbc24d46eef31385f
Anand Chembarpu <anandchembarpu@gmail.com>
2019-09-21 22:24:11 +0530

    getMovieDetails is now called from the movie page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,15 +2,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { getNowPlaying, getMovieDetails } from "../apiCalls";
+import { getNowPlaying } from "../apiCalls";
 
 class Home extends Component {
   componentDidMount = () => {
     getNowPlaying();
-  };
-
-  setMovieDetails = id => {
-    getMovieDetails(id);
   };
 
   render() {
@@ -20,9 +16,7 @@ class Home extends Component {
       <>
         {nowPlaying.map(movie => (
           <Link to={`/movie/${movie.id}`}>
-            <p key={movie.id} onClick={() => this.setMovieDetails(movie.id)}>
-              {movie.original_title}
-            </p>
+            <p key={movie.id}>{movie.original_title}</p>
           </Link>
         ))}
       </>
--- client/src/pages/Movie.js
+++ client/src/pages/Movie.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getMovieDetails } from "../apiCalls";
 
-export default class Movie extends Component {
+class Movie extends Component {
+  componentDidMount = () => {
+    const { id } = this.props.match.params;
+    getMovieDetails(id);
+  };
+
   render() {
+    const { details } = this.props;
+
     return (
       <>
-        <h1>TITLE</h1>
+        <h1>{details.original_title}</h1>
       </>
     );
   }
 }
+
+const mapStateToProps = ({ api }) => {
+  return {
+    details: api.details
+  };
+};
+
+export default connect(mapStateToProps)(Movie);
