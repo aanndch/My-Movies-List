bcc4e12df848dc988d72ff84532a2ee4da7f4071
Anand Chembarpu <anandchembarpu@gmail.com>
2019-09-21 23:29:00 +0530

    Changed import notation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography
+} from "@material-ui/core";
 
 import { getNowPlaying } from "../apiCalls";
 
