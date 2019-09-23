a754ca524a8224882ee6d99455dfb263898829d9
Anand Chembarpu <anandchembarpu@gmail.com>
2019-09-24 00:13:17 +0530

    Created Search Header

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
+import { Input } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
 
-export default class Search extends Component {
+import "./Search.css";
+
+export default class SearchHeader extends Component {
   render() {
-    return <div>SEARCH</div>;
+    return (
+      <div className="search-container">
+        <h1 className="logo">MML</h1>
+        <div className="search-part">
+          <Search className="search-icon" />
+          <Input
+            className="search-input"
+            disableUnderline={true}
+            placeholder="Search for a movie"
+          />
+        </div>
+        <div className="picture-part">
+          <img
+            className="profile-pic"
+            src="https://www.instituteofphotography.in/wp-content/uploads/2015/05/dummy-profile-pic.jpg"
+            alt="Picture"
+            height={50}
+            width={50}
+          />
+        </div>
+      </div>
+    );
   }
 }
