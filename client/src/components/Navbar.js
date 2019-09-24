0bd3fe5cc9beef103235aac88c7317b8a0709be2
Anand Chembarpu <anandchembarpu@gmail.com>
2019-09-24 09:30:43 +0530

    Removed constructor

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,10 +6,6 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="navbar">
