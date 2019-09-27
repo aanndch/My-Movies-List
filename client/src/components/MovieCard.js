57081a6331d7c2f72a540e21b489e9a290064b0d
Anand Chembarpu <anandchembarpu@gmail.com>
2019-09-27 14:42:53 +0530

    Changed Movie Card to stateless component

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { Card } from "@material-ui/core";
 
-function MovieCard({ title, poster, rating }) {
-  return (
-    <Card className="movie-card">
-      <img
-        className="movie-image"
-        src={`https://image.tmdb.org/t/p/w500${poster}`}
-        alt={title}
-      />
-      <div className="movie-rating">{rating}</div>
-    </Card>
-  );
-}
+const MovieCard = ({ title, poster, rating }) => (
+  <Card className="movie-card">
+    <img
+      className="movie-image"
+      src={`https://image.tmdb.org/t/p/w500${poster}`}
+      alt={title}
+    />
+    <div className="movie-rating">{rating}</div>
+  </Card>
+);
 
 export default MovieCard;
