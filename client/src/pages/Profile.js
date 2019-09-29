7e7bde7f9901d4d4e54d4a3f3c01a04722d544ae
Anand Chembarpu <anandchembarpu@gmail.com>
2019-09-29 19:10:54 +0530

    Started work on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,71 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { TextField } from "@material-ui/core";
+
 import { getSearchedUserInfo } from "../userInteractions";
 
-export default class Profile extends Component {
+import "./Profile.css";
+
+class Profile extends Component {
   componentDidMount = () => {
     const { username } = this.props.match.params;
     getSearchedUserInfo(username);
   };
 
   render() {
+    const { details } = this.props;
+
     return (
-      <div>
-        <h1>PROFILE</h1>
+      <div className="profile-container">
+        <div className="top-part">
+          <img
+            src={details.image}
+            alt={`${details.username}'s pic`}
+            className="profile-pic"
+          />
+          <h1>{details.username}</h1>
+        </div>
+        <div className="profile-inputs">
+          <TextField
+            label="First Name"
+            className="profile-input"
+            // value={values.name}
+            // onChange={handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            label="Last Name"
+            className="profile-input"
+            // value={values.name}
+            // onChange={handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            label="Country"
+            className="profile-input"
+            // value={values.name}
+            // onChange={handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            label="Gender"
+            className="profile-input"
+            // value={values.name}
+            // onChange={handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+        </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ searchedUser }) => ({
+  details: searchedUser
+});
+
+export default connect(mapStateToProps)(Profile);
