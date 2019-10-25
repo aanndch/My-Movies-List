72436c0639390a164a372faba167a7f1e12912d9
Anand Chembarpu <anandchembarpu@gmail.com>
2019-10-25 10:24:10 +0530

    Made passing of movie rating optional

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Card } from "@material-ui/core";
 
 const MovieCard = ({ title, poster, rating }) => (
-  <Card className="movie-card">
+  <Card className="movie-card profile-movie">
     <img
       className="movie-image"
       src={`https://image.tmdb.org/t/p/w500${poster}`}
       alt={title}
     />
-    <div className="movie-rating">{rating}</div>
+    {rating && <div className="movie-rating">{rating}</div>}
   </Card>
 );
 
