ec39e39e435a0f8366938e51cd053ed514a67fda
Anand Chembarpu <anandchembarpu@gmail.com>
2019-10-27 22:38:14 +0530

    Added Mobile discover component and fixed it accordingly

--- client/src/pages/Discover/Discover.js
+++ client/src/pages/Discover/Discover.js
@@ -1,20 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {
-  CircularProgress,
-  FormControlLabel,
-  Checkbox,
-  Button
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
 
 import { setFilters } from "../../actions/userActions";
 import { getMoviesByGenres } from "../../apiCalls";
-import { genresDB } from "../../data/movieGenres";
 
-import MovieCard from "../../components/MovieCard";
-
-import "./Discover.css";
+import DiscoverMobile from "./DiscoverMobile";
+import DiscoverDesktop from "./DiscoverDesktop";
 
 class Discover extends Component {
   componentDidMount() {
@@ -53,70 +44,29 @@ class Discover extends Component {
     const { genres, isLoading } = this.props;
     const { filteredMovies } = this.props;
 
-    return (
-      <div className="discover-container">
-        <div className="discover-movies">
-          <h1>Discover</h1>
-          {isLoading ? (
-            <CircularProgress className="loader" />
-          ) : (
-            <div className="filtered-movies">
-              {filteredMovies &&
-                filteredMovies.map(movie => (
-                  <Link
-                    key={movie.id}
-                    to={`/movie/${movie.id}`}
-                    style={{ textDecoration: "none", margin: "0.8rem" }}
-                  >
-                    <MovieCard
-                      key={movie.id}
-                      title={movie.title}
-                      poster={movie.poster_path}
-                      rating={movie.vote_average}
-                    />
-                  </Link>
-                ))}
-            </div>
-          )}
-        </div>
-        <div className="select-genres">
-          <h1>Filter :</h1>
-          <div className="genre-checkboxes">
-            {genresDB.map(genre => (
-              <FormControlLabel
-                key={genre.id}
-                control={
-                  <Checkbox
-                    color="primary"
-                    checked={genres.includes(genre.id)}
-                    onChange={this.handleGenreSelect}
-                    value={genre.id}
-                    className="genre-checkbox"
-                  />
-                }
-                label={genre.name}
-                className="checkbox-label"
-              />
-            ))}
-          </div>
-          <Button
-            className="filter-button"
-            color="primary"
-            size="large"
-            variant="contained"
-            onClick={this.getMovies}
-          >
-            FILTER
-          </Button>
-        </div>
-      </div>
-    );
+    if (window.innerWidth < 500) {
+      return (
+        <DiscoverMobile
+          genres={genres}
+          isLoading={isLoading}
+          filteredMovies={filteredMovies}
+          handleGenreSelect={this.handleGenreSelect}
+          getMovies={this.getMovies}
+        />
+      );
+    } else {
+      return (
+        <DiscoverDesktop
+          genres={genres}
+          isLoading={isLoading}
+          filteredMovies={filteredMovies}
+          handleGenreSelect={this.handleGenreSelect}
+          getMovies={this.getMovies}
+        />
+      );
+    }
   }
 }
-
-// const mapDispatchToProps = dispatch => ({
-//   setFilters: filters => dispatch(setFilters(filters))
-// });
 
 const mapStatetoProps = ({ api, user, loading }) => ({
   filteredMovies: api.filteredMovies,
@@ -124,7 +74,4 @@ const mapStatetoProps = ({ api, user, loading }) => ({
   isLoading: loading.isLoading
 });
 
-export default connect(
-  mapStatetoProps
-  // mapDispatchToProps
-)(Discover);
+export default connect(mapStatetoProps)(Discover);
---
+++ client/src/pages/Discover/DiscoverMobile.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  CircularProgress,
+  FormControlLabel,
+  Checkbox,
+  Button
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+import { genresDB } from "../../data/movieGenres";
+
+import MovieCard from "../../components/MovieCard";
+
+import "./DiscoverMobile.css";
+
+const DiscoverMobile = props => {
+  const { genres, isLoading, filteredMovies, handleGenreSelect, getMovies } = props;
+
+  return (
+    <div className="discover-container">
+      <div className="discover-movies">
+        <h1>Discover</h1>
+        {isLoading ? (
+          <CircularProgress className="loader" />
+        ) : (
+          <div className="filtered-movies">
+            {filteredMovies &&
+              filteredMovies.map(movie => (
+                <Link
+                  key={movie.id}
+                  to={`/movie/${movie.id}`}
+                  style={{ textDecoration: "none", margin: "0.8rem" }}
+                >
+                  <MovieCard
+                    key={movie.id}
+                    title={movie.title}
+                    poster={movie.poster_path}
+                    rating={movie.vote_average}
+                  />
+                </Link>
+              ))}
+          </div>
+        )}
+      </div>
+      <div className="select-genres">
+        <h1>Filter :</h1>
+        <div className="genre-checkboxes">
+          {genresDB.map(genre => (
+            <FormControlLabel
+              key={genre.id}
+              control={
+                <Checkbox
+                  color="primary"
+                  checked={genres.includes(genre.id)}
+                  onChange={handleGenreSelect}
+                  value={genre.id}
+                  className="genre-checkbox"
+                />
+              }
+              label={genre.name}
+              className="checkbox-label"
+            />
+          ))}
+        </div>
+        <Button
+          className="filter-button"
+          color="primary"
+          size="large"
+          variant="contained"
+          onClick={getMovies}
+        >
+          FILTER
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default DiscoverMobile;
