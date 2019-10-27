4c1c1a6c501727c51658ad0940b444562039e9ce
Anand Chembarpu <anandchembarpu@gmail.com>
2019-10-27 13:33:47 +0530

    Routing changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,37 @@ import store from "./store";
 import history from "./history";
 import Routes from "./routes";
 import Navbar from "./components/Navbar";
+import NavbarMobile from "./components/NavbarMobile";
 // import Footer from "./components/Footer";
 
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Router history={history}>
-        <div style={{ display: "flex", minheight: "100vh" }}>
-          <Navbar />
-          <div className="main">
-            <Routes />
+  if (window.innerWidth < 500) {
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+          <NavbarMobile />
+          <Routes />
+          {/* <Footer /> */}
+        </Router>
+      </Provider>
+    );
+  } else {
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+          <div style={{ display: "flex", minheight: "100vh" }}>
+            <Navbar />
+            <div className="main">
+              <Routes />
+            </div>
           </div>
-        </div>
-        {/* <Footer /> */}
-      </Router>
-    </Provider>
-  );
+          {/* <Footer /> */}
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
