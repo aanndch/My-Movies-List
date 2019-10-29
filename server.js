ab873fb691d196acf671c0e910e33e1a2b9b8a49
Anand Chembarpu <anandchembarpu@gmail.com>
2019-10-29 21:11:44 +0530

    Added build commands

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const users = require("./routes/api/users");
 const searchUser = require("./routes/api/searchUser");
 const lists = require("./routes/api/lists");
 const updateUser = require("./routes/api/updateUser");
+const path = require("path");
 
 dotenv.config();
 
@@ -31,6 +32,15 @@ app.use("/api/users", users);
 app.use("/api/users/search", searchUser);
 app.use("/api/users/update", updateUser);
 app.use("/api/users/lists", lists);
+
+// Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
