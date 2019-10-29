a606f744cf75e0696ed2cb0eb0eaad7ae327c674
Anand Chembarpu <anandchembarpu@gmail.com>
2019-10-29 22:56:55 +0530

    Minor change

--- server.js
+++ server.js
@@ -42,6 +42,4 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server started!`));
