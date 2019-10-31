1963982a43fa8d3e869e3843d3d7ac80fcaebecc
Anand Chembarpu <anandchembarpu@gmail.com>
2019-10-31 22:03:16 +0530

    Minor changes

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use("/api/users/lists", lists);
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server started!`));
+app.listen(process.env.PORT, () => console.log(`Server started!`));
